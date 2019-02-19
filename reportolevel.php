675780e0ff2a217f5598542ea3dccf54df780a83
felixkiryowa <franciskiryowa68@gmail.com>
2019-02-19 10:20:59 +0300

    Designed o level report card

---
+++ reportolevel.php
@@ -0,0 +1,318 @@
+<html>
+   <head>
+       
+    <!-- Bootstrap -->
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="build/css/report.css"    rel="stylesheet">
+   </head>
+   <body>
+        <page size="A4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                        <img src="images/vienna.jpg" alt="school_badge" class="school_badge"/>
+                    </div>
+                    <div class="col-md-6">
+                        <h1  class="text_color  school_name">VIENNA HIGH SCHOOL KABOWA</h1>
+                        <p align="center" class="text_color"><b>P.O.BOX 6536, KLA,</b></p>
+                        <P align="center" class="text_color"><b>TEL: 0782-575302 / 0756-066979</b></P>
+                        <h3 align="center" class="terminal_report">'O' LEVEL TERMINAL REPORT</h3>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                         <p  class="stud_name"><b>NAME OF STUDENT .............................................................................................</b></p>
+                    </div>
+                    <div class="col-md-4">
+                        <p class="stud_name"><b>CLASS ......................................................</b></p>
+                    </div>
+
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                         <p  class="stud_name"><b>POSITION / GRADE .............................</b></p>
+                    </div>
+                    <div class="col-md-3">
+                        <p class="stud_name"><b>TERM ..........................................</b></p>
+                    </div>
+                    <div class="col-md-3">
+                        <p class="stud_name"><b>YEAR ........................................................</b></p>
+                    </div>
+
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <table style="width:850px;">
+                            <tr>
+                               <th style="width:200px;"><b><p align="center">SUBJECT</p></b></th>
+                               <th><b><p align="center" style="font-size:11px;">M.O.T <br>WORK 100%</b></p></th>
+                               <th><b><p align="center" style="font-size:11px;">E.O.T <br>WORK 100%</b></p></th>
+                               <th><b><p align="center">TOTAL</p></b></th>
+                               <th><b><p align="center">AVERAGE</p></b></th>
+                               <th><b><p align="center" >GRADE</p></b></th>
+                               <th><b><p align="center">REMARKS</p></b></th>
+                               <th><b><p align="center">INITIALS</p></b></th>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>ENGLISH</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>MATHEMATICS</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>GEOGRAPHY</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>COMMERCE</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>HISTORY</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>FINE ART</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>BIOLOGY</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>CHEMISTRY</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>PHYSICS</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>AGRICULTURE</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>C.R.E</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>ENTREPRENUERSHIP</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>KISWAHILI</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>I.R.E</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>COMPUTER</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>LUGANDA</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>LUGANDA</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b>ENGLISH LITERATURE</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr style="height:40px;">
+                                <td><b style="font-size:22px;">TOTAL</b></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+            </div>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <p class="stud_name"><b>Class teacher's Comment : ..........................................................................................................................................................................</b></p>
+                        <p class="stud_name"><b>...............................................................................................................................................<b class="stud_name">Signature :</b> .......................................................</b></p>
+                        <p class="stud_name"><b>Head Teacher's Comment : ..........................................................................................................................................................................</b></p>
+                        <p class="stud_name"><b>................................................................................................................................................<b>Signature: .......................................................</b></b></p>
+                        <p class="stud_name"><b>Next term begins ..............................................................................................................<b>and ends on .......................................................</b></b></p>
+                        <p class="stud_name"><b>School fees ....................................................................................................................................................................................................</p>
+                    </div>
+                </div>
+            </div>
+            <p align="center" style="color:blue;">GRADING</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <table style="width:850px;" class="special_table">
+                            <tr>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">85%-100%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">84%-75%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">74%-69%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">68%-60%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">59%-55%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">54%-50%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">49%-45%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">44%-40%</b></p></th>
+                                <th style="color:blue;"><b><p align="center" style="font-size:11px;">39%-00%</b></p></th>
+                            </tr> 
+                            <tr style="height:40px;">
+                                <td><b>D1</b></td>
+                                <td><b>D2</b></td>
+                                <td><b>C3</b></td>
+                                <td><b>C4</b></td>
+                                <td><b>C5</b></td>
+                                <td><b>C6</b></td>
+                                <td><b>P7</b></td>
+                                <td><b>P8</b></td>
+                                <td><b>F9</b></td>
+                            </tr> 
+                        </table>
+                        <br>
+                        <p align="center" style="font-style:italic;color:green;">NOTE:  This report is only valid when it has a School Stamp</p>
+                    </div>
+                </div>
+            </div>
+            
+            <br><br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+
+        </page>
+   </body>
+</html>
